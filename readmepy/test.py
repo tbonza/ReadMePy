348130b4ab1c4cabc7f49b45978f7c64dc19d5e7
Tyler Brown <tylers.pile@gmail.com>
2013-07-05 17:11:15 -0400

    Update test.py
    adding a couple functions

--- readmepy/test.py
+++ readmepy/test.py
@@ -2,24 +2,28 @@
 
 def make_binary(matrix):
     # assign number or rows & columns to variables
-
-    # clean up this nonsense
-    r = num_row(matrix)
-    c = num_col(matrix, 1)
-    for i in range(r):
-        print r
-        for j in range(c):
-    # with list comprehensions
-
-            if(is_blank(matrix)== True):
-                cell = 0
-            elif(is_blank(matrix)== False):
-                cell = 1
+    if(check_cell(is_blank(matrix)) == True):
+        cell = 0
+    elif(check_cell(is_blank(matrix)) == False):
+        cell = 1
                 
     return matrix
 
 def is_blank(matrix):
     # is a cell blank?
+    pass
+
+def check_cell(matrix):
+    # check each cell in matrix
+    '''
+     r = num_row(matrix)
+    c = num_col(matrix, 1)
+    for i in range(r):
+        print r
+        for j in range(c):
+    '''
+    # clean up this nonsense
+    # with list comprehensions
     pass
     
 
