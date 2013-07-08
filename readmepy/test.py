b8dcbc2583b35302c7e95e698d687468a7caee55
Bonza-Times <tylers.pile@gmail.com>
2013-07-07 20:02:13 -0400

    updated is_blank function

--- readmepy/test.py
+++ readmepy/test.py
@@ -14,20 +14,9 @@ def is_blank(matrix):
     '''
     This function assumes that when a file is read, empty cells
     will be assigned as 'None'. Given this assumption we can
-    determine whether a cell, or list item, is empty.
+    determine whether a cell, or list item, is empty. If a cell
+    is empty, assigned as 'None', then the cell will be returned
+    as False. Otherwise, the cell is returned as True.
     '''
-    if check_cell(matrix) is None:
-        return True
-    elif check_cell(matrix) is not None:
-        return False
-    else:
-        return "Error: cell is not designated as int or none"
-  
-  
-def check_cell(matrix):
-    # check each cell in matrix
-    # clean up this nonsense
-    # with list comprehensions
-    for row in range(mo.num_row(matrix)):
-        for cell in range(len(mo.get_row(matrix, row))):
-            print  mo.get_row(matrix,row)[cell], "\t"
+    bool_blank = [[cell == None for cell in row] for row in matrix]
+    return bool_blank
