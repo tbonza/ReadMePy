d3d574caf3836d8879a2f6d0c6578e5a688264f6
Bonza-Times <tylers.pile@gmail.com>
2013-07-11 19:53:22 -0400

    various functions in the testing stage

--- readmepy/test.py
+++ readmepy/test.py
@@ -1,5 +1,27 @@
-# write function "readme"
-        
+# write functions from va library
+'''
+The following functions need to be implemented from the 
+VA library in R:
 
-        
+formula
+features
+n_subset
+prob_wt
+boot_se
+nboot
+printit
+
+The following data structures need to be implemented from
+the same library
+
+hospital 
+community
+'''
+
+
+
+
+
+    
+    
 
