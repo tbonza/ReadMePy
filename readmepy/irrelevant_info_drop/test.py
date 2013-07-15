c13f33b593cc8ac5760422ec4c08aeba555875b5
Bonza-Times <tylers.pile@gmail.com>
2013-07-14 22:47:26 -0400

    started on a non-english text filter

--- readmepy/irrelevant_info_drop/test.py
+++ readmepy/irrelevant_info_drop/test.py
@@ -1,1 +1,19 @@
 # Test functions for irrelevent_info_drop
+
+# Function for non-english language text
+'''
+Sources:
+  http://www.velvetcache.org/2010/03/01/looking-up-words-in-a-dictionary-using-python
+
+'''
+from nltk.corpus import wordnet
+
+words = ["cookies","vous","hipsters","chocolate","monty"]
+
+
+for word in words:
+    synsets = wordnet.synsets(word)
+    print synsets
+
+synsets = wordnet.synsets('cookies')
+print synsets
