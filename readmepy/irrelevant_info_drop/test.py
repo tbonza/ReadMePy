140721e8f9fc5a6b6f6e37c7adb274cef3430bd6
Bonza-Times <tylers.pile@gmail.com>
2013-07-15 22:54:06 -0400

    added functions to test for non-english text

--- readmepy/irrelevant_info_drop/test.py
+++ readmepy/irrelevant_info_drop/test.py
@@ -8,12 +8,33 @@ Sources:
 '''
 from nltk.corpus import wordnet
 
+def get_test_phrase(document):
+    '''
+    This function grabs the first sentence of the document
+    in question to test if it is in english or not.
+    '''
+    pass
+   # return words
+
 words = ["cookies","vous","hipsters","chocolate","monty"]
 
+def is_english(words):
+    '''
+    Returns a list of booleans where a word is True
+    if it is english; False if it is not english.
+    Errors append 'None' to the list
+    '''
+    for word in words:
+        english = []
+        synsets = wordnet.synsets(word)
+        if word in synsets:
+            english.append(True)
+        elif word not in synsets:
+            english.append(False)
+        else:
+            english.append(None)
+    return english
 
-for word in words:
-    synsets = wordnet.synsets(word)
-    print synsets
-
+# caution: random scripting below this line
 synsets = wordnet.synsets('cookies')
 print synsets
