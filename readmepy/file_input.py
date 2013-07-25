5b184c568b00c51952e27cb0bb461469444b9430
Bonza-Times <tylers.pile@gmail.com>
2013-07-24 20:52:38 -0400

    still working on it

--- readmepy/file_input.py
+++ readmepy/file_input.py
@@ -5,24 +5,37 @@
 
 path = '/home/ty/summer_project/clintonposts/'
 
+document = path + 'control.txt'
+
 def read_local(document): 
     '''
-Reads local files into
-ReadMePy
-'''
-    f = open(document, 'r')
+    Reads local files into
+    ReadMePy by each line
+    '''
+    f = open(document, 'r'
     return f
 
+def assign_tuple(document):
+    '''
+    line becomes a tuple
+    '''
+    tuple_list = []
+    for line in read_local(document).readlines():
+        t = line
+        tuple_list.append(t)
+    return tuple_list
+             
+'''
 def tuple_list(document):
-    '''
+    
     Read each line in as a tuple
-    '''
+    
     tuple_list = []
     lines = read_local(document)
     for line in lines.readlines():
         temp = line
-        
-        tuple_list.append(
-        temp[0] = temp[1:(len(temp)-1))
-        )
+        length = len(temp) - 1
+        tuple_list.append(temp[0] = temp[1:length]
     return tuple_list
+
+'''
