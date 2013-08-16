55b09ea23e1c27d30a8f34dbbe94c91c5ecb14e7
Bonza-Times <tylers.pile@gmail.com>
2013-08-16 11:05:55 -0400

    adding test functions so we can get the show on the road

--- readmepy/feedDB/debug.py
+++ readmepy/feedDB/debug.py
@@ -4,13 +4,34 @@ import chron_job as cj
 count = len(cj.get_tablenames(RSS_link_list))
 print count
 
-for number in range(count):
-  try  
-    if cj.articles(RSS_link_list, number) > 0
-      print True, cj.get_tablenames(RSS_link_list)[number]
-    elif cj.articles(RSS_link_list, number) < 0
-      print False, cj.get_tablenames(RSS_link_list)[number]
-  except 
-    print cj.get_tablenames(RSS_link_list)[number], "returns an exception"
-      
+def test_1(count):
+  '''
+  When running the table function like this:
+  a = table(RSS_link_list) 
+  print a[0]
+  
+  Error Message returned: 
+  UnicodeEncodeError: 'ascii' codec can't encode character 
+  u'\u2019' in position 162: ordinal not in range(128)
+  '''
+  for number in range(count):
+    try:  
+      if articles(cj.RSS_link_list, number) > 0:
+        print True, cj.get_tablenames(RSS_link_list)[number]
+      elif cj.articles(RSS_link_list, number) < 0:
+        print False, cj.get_tablenames(RSS_link_list)[number]
+    except: 
+      print cj.get_tablenames(RSS_link_list)[number], "returns an exception"
+
+
+def test_2(count):
+  '''
+  When running test_1:
+    NYTBusinessDay returns an exception
+    NYTHomePage returns an exception
+    NYTWorld returns an exception
+  
+  Finding those exceptions so they can be fixed.
+  '''
+  
 
